57337dec352a0378854935bc8e8cbb92d99be860
Rendya Yuschak <rendyayuschak@gmail.com>
2025-01-02 20:52:06 +0700

    create Navbar component

---
+++ website-kapwika-cikarang-react/src/App.tsx
@@ -0,0 +1,10 @@
+import Navbar from "./RootComponent/NavbarComponent"
+
+function App() {
+  return <>
+  <Navbar/>
+  <h1> Hello World</h1>
+  </>
+}
+
+export default App
---
+++ website-kapwika-cikarang-react/src/RootComponent/NavbarComponent.tsx
@@ -0,0 +1,46 @@
+import './NavbarComponent.css';
+import NavbarLogo from '../assets/LogoNavbarComponent.jpg'
+
+const Navbar = () => {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img src={NavbarLogo} alt="Logo" className="logo" width="323" height="48"/>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item mx-3">
+                <a className="nav-link" href="#home">Home</a>
+              </li>
+              <li className="nav-item mx-3">
+                <a className="nav-link" href="#company-profile">Company Profile</a>
+              </li>
+              <li className="nav-item mx-3">
+                <a className="nav-link" href="#services">Services</a>
+              </li>
+              <li className="nav-item mx-3">
+                <a className="nav-link" href="#about">About</a>
+              </li>
+              <li className="nav-item mx-3">
+                <a className="nav-link" href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  };
+  
+export default Navbar;
